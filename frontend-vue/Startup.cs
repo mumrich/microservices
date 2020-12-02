0aafb258e0ab0da44481dfbbadcecf80b98654d5
Hannes Zbinden <hanneszbinden@outlook.com>
2020-12-02 16:00:50 +0100

    add new client-app.

--- frontend-vue/Startup.cs
+++ frontend-vue/Startup.cs
@@ -1,20 +1,33 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.SpaServices;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+
+using VueCliMiddleware;
 
 namespace frontend_vue
 {
   public class Startup
   {
-    // This method gets called by the runtime. Use this method to add services to the container.
-    // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+    private const string CLIENT_APP_NAME = "client-app";
+    /// <summary>
+    /// This method gets called by the runtime. Use this method to add services to the container.
+    /// For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+    /// </summary>
+    /// <param name="services"></param>
     public void ConfigureServices(IServiceCollection services)
     {
+      // NOTE: PRODUCTION Ensure this is the same path that is specified in your webpack output
+      services.AddSpaStaticFiles(opt => opt.RootPath = $"{CLIENT_APP_NAME}/dist");
+      services.AddControllers();
     }
 
-    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    /// <summary>
+    /// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    /// </summary>
+    /// <param name="app"></param>
+    /// <param name="env"></param>
     public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
     {
       if (env.IsDevelopment())
@@ -22,10 +35,20 @@ namespace frontend_vue
         app.UseDeveloperExceptionPage();
       }
 
+      // PRODUCTION uses webpack static files
+      app.UseSpaStaticFiles();
       app.UseRouting();
       app.UseEndpoints(endpoints =>
       {
-        endpoints.MapGet("/", async context => await context.Response.WriteAsync("Hello World!"));
+        endpoints.MapControllers();
+        endpoints.MapToVueCliProxy(
+          "{*path}",
+          new SpaOptions { SourcePath = CLIENT_APP_NAME },
+          npmScript: env.IsDevelopment() ? "serve" : null,
+          regex: "Compiled successfully",
+          forceKill: true,
+          wsl: false, // Set to true if you are using WSL on windows. For other operating systems it will be ignored
+          https: false);
       });
     }
   }
